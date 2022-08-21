b31cc18bdfa9e40fa291c8f070f5bf522e6e3939
Sergey Parshin <quarck@gmail.com>
2022-08-21 15:58:02 +0100

    Now in 9 colours

--- aperture_renderer/aperture_renderer.cpp
+++ aperture_renderer/aperture_renderer.cpp
@@ -15,6 +15,18 @@ constexpr float PI = 3.14159265359f;
 constexpr float GENERAL_MULTIPLIER = 10000.0;
 constexpr float BRIGHT_RATIO = 90000.0; // basically defines how much "light" we want to see in the final render
 
+struct lambda_profile
+{
+	float lambda; // wavelength 
+	float velocity; // wave velocity through the medium
+
+	lambda_profile(float lambda)
+		: lambda{ lambda }
+		, velocity{ static_cast<float>(lambda / (2.0 * PI)) }
+	{
+	}
+};
+
 struct aperture
 {
 	// i = y * Width + x
@@ -25,20 +37,20 @@ struct aperture
 	int height;
 
 	//float omega; // we set such a value of v, so omega == 1.0 all the time. All we care is the wavelenght 
-	float lambda; // wavelength 
-	float velocity; // wave velocity through the medium
+	//float lambda; // wavelength 
+	//float velocity; // wave velocity through the medium
 	float d; // distance between z==0 and the 'screen'
 	
 	// R is the radius of the 'lense', with the centre at (Width/2.0, Height/2.0, R), it affects the 
 	// curvature of 'ZValues'
 
 	aperture(std::vector<unsigned char> img, 
-		int width, int height, float d, float R, float lambda)
+		int width, int height, float d, float R/*, float lambda*/)
 		: width { width }
 		, height { height }
 		, d { d }
-		, lambda { lambda }
-		, velocity { static_cast<float>(lambda / (2.0 * PI)) } // thus f == 1/(2*pi), omega == 1.0
+		//, lambda { lambda }
+		//, velocity { static_cast<float>(lambda / (2.0 * PI)) } // thus f == 1/(2*pi), omega == 1.0
 	{
 		intensity_mask.resize(width * height);
 		z_values.resize(width * height);
@@ -68,7 +80,7 @@ struct aperture
 		}
 	}
 
-	float diff_value(int x, int y)
+	float diff_value(const lambda_profile& lp, int x, int y)
 	{
 		float accum_a = 0;
 		float accum_b = 0;
@@ -87,7 +99,7 @@ struct aperture
 				auto l_sqr = std::pow(ax - x, 2.0) + std::pow(ay - y, 2.0) + std::pow(z - d, 2.0);
 
 				float ivq = (float)(GENERAL_MULTIPLIER /* * intensity*/ / l_sqr);
-				auto d_tv = std::sqrt(l_sqr) / velocity;
+				auto d_tv = std::sqrt(l_sqr) / lp.velocity;
 
 				accum_a += (float)(ivq * std::cos(d_tv));
 				accum_b += (float)(ivq * std::sin(d_tv));
@@ -132,9 +144,15 @@ int main(int argc, char* argv[])
 		std::cerr << "aperture_renderer <input.png> <output.png> [<d>] [<R>] [<lambda>]" << std::endl;
 		std::cerr << "default values are: d = 200 (px), R = 1000 (px), lambda = 0.55" << std::endl;
 		std::cerr << "Note: lambda defines the wavelength for the mid-spectrum, rendering will be done using three values:" << std::endl;
-		std::cerr << "lambda_red = lambda * 1.2" << std::endl;
-		std::cerr << "lambda_green = lambda * 1.0" << std::endl;
-		std::cerr << "lambda_blue = lambda / 1.2" << std::endl;
+		std::cerr << "lambda_red1 = lambda * 1.21" << std::endl;
+		std::cerr << "lambda_red2 = lambda * 1.16" << std::endl;
+		std::cerr << "lambda_red4 = lambda * 1.10" << std::endl;
+		std::cerr << "lambda_green1 = lambda * 1.05" << std::endl;
+		std::cerr << "lambda_green2 = lambda * 1.0" << std::endl;
+		std::cerr << "lambda_green3 = lambda * 0.95" << std::endl;
+		std::cerr << "lambda_blue1 = lambda * 0.91" << std::endl;
+		std::cerr << "lambda_blue2 = lambda * 0.86" << std::endl;
+		std::cerr << "lambda_blue3 = lambda * 0.82" << std::endl;
 		std::cerr << "(go to the source code to change those multipliers)";
 		return -1;
 	}
@@ -144,9 +162,7 @@ int main(int argc, char* argv[])
 
 	float d = argc >= 4 ? std::atof(argv[3]) : 200.0;
 	float R = argc >= 5 ? std::atof(argv[4]) : 1000.0;
-	float lambda_g = argc >= 6 ? std::atof(argv[5]) : .55;
-	float lambda_r = lambda_g * 1.2f;
-	float lambda_b = lambda_r / 1.2f;
+	float lambda = argc >= 6 ? std::atof(argv[5]) : .55;
 
 	std::vector<unsigned char> data;
 	unsigned width;
@@ -159,13 +175,16 @@ int main(int argc, char* argv[])
 
 	ThreadGrid _grid{ numWorkerThreads };
 
-	aperture ap_r{ data, static_cast<int>(width), static_cast<int>(height), d, R, lambda_r };
-	aperture ap_g{ data, static_cast<int>(width), static_cast<int>(height), d, R, lambda_g };
-	aperture ap_b{ data, static_cast<int>(width), static_cast<int>(height), d, R, lambda_b };
+	aperture ap{ data, static_cast<int>(width), static_cast<int>(height), d, R };
 
-	std::vector<float> out_raw_r(width * height);
-	std::vector<float> out_raw_g(width * height);
-	std::vector<float> out_raw_b(width * height);
+	std::vector<lambda_profile> lps;
+	std::vector<std::vector<float>> out_raw(9); //  width* height);
+
+	for (int i = 0; i < 9; i++)
+	{
+		lps.push_back(lambda_profile{ (std::powf(1.05f, 4 - i) * lambda) });
+		out_raw[i].resize(width * height);
+	}
 
 	std::mutex m;
 	int progress = 0;
@@ -177,36 +196,21 @@ int main(int argc, char* argv[])
 			int from = thread_idx * slice;
 			int to = std::min<int>(height, (thread_idx + 1) * slice);
 
-			for (int y = from; y < to; ++y)
+			for (int i = 0; i < 9; ++i)
 			{
-				for (int x = 0; x < width; x++)
-				{
-					out_raw_r[y * width + x] = ap_r.diff_value(x, y) ;
-				}
-				std::lock_guard l{ m };
-				report_progress(++progress, height * 3);
-			}
+				const auto& lp = lps[i];
+				auto& dst = out_raw[i];
 
-			for (int y = from; y < to; ++y)
-			{
-				for (int x = 0; x < width; x++)
+				for (int y = from; y < to; ++y)
 				{
-					out_raw_g[y * width + x] = ap_g.diff_value(x, y);
+					for (int x = 0; x < width; x++)
+					{
+						dst[y * width + x] = ap.diff_value(lp, x, y);
+					}
+					std::lock_guard l{ m };
+					report_progress(++progress, height * 9);
 				}
-				std::lock_guard l{ m };
-				report_progress(++progress, height * 3);
 			}
-
-			for (int y = from; y < to; ++y)
-			{
-				for (int x = 0; x < width; x++)
-				{
-					out_raw_b[y * width + x] = ap_b.diff_value(x, y);
-				}
-				std::lock_guard l{ m };
-				report_progress(++progress, height * 3);
-			}
-
 		});
 
 	float sum{ 0 };
@@ -215,8 +219,10 @@ int main(int argc, char* argv[])
 	{
 		for (int x = 0; x < width; x++)
 		{
-			float v = out_raw_r[y * width + x] + out_raw_g[y * width + x] + out_raw_b[y * width + x];
-			sum += v / 3.0;
+			float v = 0;
+			for (int i = 0; i < 9; ++i)
+				v += out_raw[i][y * width + x];
+			sum += v / 9.0;
 		}
 	}
 
@@ -230,9 +236,14 @@ int main(int argc, char* argv[])
 		{
 			int i_offs = y * width + x;
 			int o_offs = 4 * i_offs;
-			out[o_offs + 0] = std::min(255u, static_cast<unsigned>(255.0 * out_raw_r[i_offs] / max));
-			out[o_offs + 1] = std::min(255u, static_cast<unsigned>(255.0 * out_raw_g[i_offs] / max));
-			out[o_offs + 2] = std::min(255u, static_cast<unsigned>(255.0 * out_raw_b[i_offs] / max));
+
+			float r = 255.0f * (out_raw[0][i_offs] + out_raw[1][i_offs] + out_raw[2][i_offs]) / 3.0f / max;
+			float g = 255.0f * (out_raw[3][i_offs] + out_raw[4][i_offs] + out_raw[5][i_offs]) / 3.0f / max;
+			float b = 255.0f * (out_raw[6][i_offs] + out_raw[7][i_offs] + out_raw[8][i_offs]) / 3.0f / max;
+
+			out[o_offs + 0] = std::min(255u, static_cast<unsigned>(r));
+			out[o_offs + 1] = std::min(255u, static_cast<unsigned>(g));
+			out[o_offs + 2] = std::min(255u, static_cast<unsigned>(b));
 			out[o_offs + 3] = 255;
 		}
 	}
